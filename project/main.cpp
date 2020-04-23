94b6e476da78f1fa17dde7d14c2f99c3c7f783e6
belchenkovova <49374370+belchenkovova@users.noreply.github.com>
2020-04-24 00:02:13 +0300

    Flags : resolution, demo

--- project/main.cpp
+++ project/main.cpp
@@ -3,22 +3,28 @@
 #include "particle_system/particle_system.h"
 #include "external/CLI11.hpp"
 
-class					final
+class							final
 {
 public :
-						final() = default;
-						~final() = default;
+								final() = default;
+								~final() = default;
 
-	bool				process_options(int argc, char **argv)
+	bool						process_options(int argc, char **argv)
 	{
-		auto		app = CLI::App("");
+		std::string				demo_string;
+		std::string				map_string;
+		std::string				resolution_string;
+
+		auto					app = CLI::App("");
 
 		app.set_help_flag();
 
-		app.add_option("--map", map_file);
+		app.add_option("--demo", demo_string);
+		app.add_option("--map", map_string);
+		app.add_option("--resolution", resolution_string);
 
-		auto		flag_help = app.add_flag("--help");
-		auto		flag_fps = app.add_flag("--fps");
+		auto					flag_help = app.add_flag("--help");
+		auto					flag_fps = app.add_flag("--fps");
 
 		try
 		{
@@ -41,17 +47,43 @@ public :
 				<< "Keys : " << std::endl
 				<< std::endl;
 			return (false);
-
 		}
 
 		engine::core::print_fps = *flag_fps ? true : false;
 
+		if (not demo_string.empty())
+			map = "maps/demo_" + demo_string + ".json";
+		else
+			map = map_string;
+
+		if (not resolution_string.empty())
+		{
+			std::stringstream	stream(resolution_string);
+
+			auto				extract_int = [&stream]()
+			{
+				int value;
+
+				if ((stream >> value).fail())
+					throw (common::exception("Final : Bad resolution value"));
+				return (value);
+			};
+
+			resolution.emplace();
+			resolution->x = extract_int();
+			if (stream.peek() != ',')
+				throw (common::exception("Final : Bad resolution pattern"));
+			stream.ignore();
+			resolution->y = extract_int();
+		}
+
 		return (true);
 	}
 
-	static void			setup_static()
+	void						setup_static()
 	{
-		engine::core::initial_window_size = engine::ivec2(1280, 720);
+		engine::core::initial_window_size =
+			resolution ? *resolution : engine::ivec2(1280, 720);
 		engine::core::window_name = "Particle System";
 		engine::core::use_multisampling = true;
 		engine::core::use_blending = true;
@@ -69,39 +101,40 @@ public :
 		computer::core::use_OpenGL = true;
 	}
 
-	void			setup_dynamic()
+	void						setup_dynamic()
 	{
 		engine.emplace();
 		computer.emplace();
 		system.emplace(*engine, *computer);
 
 		engine->attach_renderer(system->receive_renderer());
-		system->build(map_file);
+		system->build(map);
 	}
 
-	void			run()
+	void						run()
 	{
 		engine->start();
 	}
 
 private :
 
-	using			engine_type = std::optional<engine::core>;
-	using			computer_type = std::optional<computer::core>;
-	using			system_type = std::optional<particle_system::manager>;
+	using						engine_type = std::optional<engine::core>;
+	using						computer_type = std::optional<computer::core>;
+	using						system_type = std::optional<particle_system::manager>;
 
-	std::string		map_file;
+	std::string					map;
+	std::optional<glm::ivec2>	resolution;
 
-	engine_type		engine;
-	computer_type	computer;
-	system_type		system;
+	engine_type					engine;
+	computer_type				computer;
+	system_type					system;
 };
 
-int					main(int argc, char **argv)
+int								main(int argc, char **argv)
 {
 	try
 	{
-		final		final;
+		final					final;
 
 		if (!final.process_options(argc, argv))
 			return (0);
